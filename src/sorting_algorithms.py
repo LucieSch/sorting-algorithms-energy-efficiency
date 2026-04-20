147debd512e3ca9af0bbe83e44a12caf49ff4e05
Lucie Schwendrat <lucie.schwendrat@gmail.com>
2026-04-20 14:12:34 +0200

    Implement quick and tim sort

--- src/sorting_algorithms.py
+++ src/sorting_algorithms.py
@@ -35,3 +35,17 @@ def merge(left, right):
     result.extend(right[j:])
 
     return result
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+def tim_sort(arr):
+    return sorted(arr) # Hybrid sorting algorithm derived from insertion sort and merge sort
